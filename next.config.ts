4a35e30cc4a11144af8e570dd1c23121af1a9ea2
Ahmed Bahnasy <engineerahmedbahnasy@gmail.com>
2025-07-15 10:38:42 +0300

    feat: added an ignore build errors flag for typescript in the next config file

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ const { i18n } = require("./next-i18next.config");
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-
+  ignoreBuildErrors: true,
   // SCSS configuration
   sassOptions: {
     includePaths: ["./styles", "./components"],
